9bf884126ffa0f952e2a4cbcfb39b3f1bde7c3f3
Michael Gault Jr <8020534+michaelgaultjr@users.noreply.github.com>
2024-04-24 23:27:21 -0400

    Increase zoom accuracy

--- lib/src/widgets/preview/awesome_camera_gesture_detector.dart
+++ lib/src/widgets/preview/awesome_camera_gesture_detector.dart
@@ -67,7 +67,7 @@ class AwesomeCameraGestureDetector extends StatefulWidget {
 class _AwesomeCameraGestureDetector
     extends State<AwesomeCameraGestureDetector> {
   double _zoomScale = 0;
-  final double _accuracy = 0.01;
+  final double _accuracy = 0.0025;
   double? _lastScale;
 
   Offset? _tapPosition;
